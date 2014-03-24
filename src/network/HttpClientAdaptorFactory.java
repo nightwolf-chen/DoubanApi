77351d2aece6475e41584ac7b69c7870529175fb
Bruce Chen <466202783@qq.com>
2014-03-24 18:13:11 +0800

    This is a folk doubanfm api in java

---
+++ src/network/HttpClientAdaptorFactory.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package network;
+
+import org.apache.http.HttpHost;
+
+/**
+ *
+ * @author bruce
+ */
+public class HttpClientAdaptorFactory {
+    public static HttpClientAdaptor createDefaultHttpClientAdaptor(String connectionEncode){
+        return new HttpClientAdaptor(connectionEncode);
+    }
+    
+     public static HttpClientAdaptor createDefaultHttpClientAdaptor(){
+         return new HttpClientAdaptor();
+    }
+}
